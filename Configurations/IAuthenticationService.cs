189741d06f6b533e8c00791e79e14643decc456b
Felipe Fadil Caetano <felipefadil@gmail.com>
2021-06-24 21:29:29 -0300

    Projeto finalizado

---
+++ Configurations/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using static curso.api.Controllers.UsuarioController;
+
+namespace curso.api.Configurations
+{
+    public interface IAuthenticationService
+    {
+        string GerarToken(UsuarioViewModelOutput usuarioViewModelOutput);
+    }
+}
---
+++ Migrations/20210624001810_Base-inicial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace curso.api.Migrations
+{
+    public partial class Baseinicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_USUARIO",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_USUARIO", x => x.Codigo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TC_CURSO",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CodigoUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TC_CURSO", x => x.Codigo);
+                    table.ForeignKey(
+                        name: "FK_TC_CURSO_TB_USUARIO_CodigoUsuario",
+                        column: x => x.CodigoUsuario,
+                        principalTable: "TB_USUARIO",
+                        principalColumn: "Codigo",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TC_CURSO_CodigoUsuario",
+                table: "TC_CURSO",
+                column: "CodigoUsuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TC_CURSO");
+
+            migrationBuilder.DropTable(
+                name: "TB_USUARIO");
+        }
+    }
+}
